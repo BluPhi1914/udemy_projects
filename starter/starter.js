59ebb2f607df5c797614cc52710b5d0121108bfe
Brad Sylvester <sylvester.brad@gmail.com>
2022-04-01 22:43:15 -0700

    Udemy JSFundmnts Pt1

---
+++ starter/starter.js
@@ -0,0 +1,17 @@
+let country = 'South Korea';
+let continent = 'Asia';
+let population = '25 Million people';
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof language);
+console.log(typeof population);
+console.log(typeof country);
+
+
